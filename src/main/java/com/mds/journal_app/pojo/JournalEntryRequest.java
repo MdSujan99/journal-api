ae20e8eb2aa8649d2cc1953a0b20b9c8323a17f6
MdSujan99 <khans8962@gmail.com>
2025-07-25 01:14:05 +0530

    add builder pattern to JournalEntryRequest for improved object creation

--- src/main/java/com/mds/journal_app/pojo/JournalEntryRequest.java
+++ src/main/java/com/mds/journal_app/pojo/JournalEntryRequest.java
@@ -1,11 +1,13 @@
 package com.mds.journal_app.pojo;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class JournalEntryRequest {
   String textContent;
 }
