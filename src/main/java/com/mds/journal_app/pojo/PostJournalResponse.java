4f1c4ea95eb4b685d7619f25c3e459ff84903fe0
MdSujan99 <khans8962@gmail.com>
2025-07-27 16:16:17 +0530

    rename property for better readability

--- src/main/java/com/mds/journal_app/pojo/PostJournalResponse.java
+++ src/main/java/com/mds/journal_app/pojo/PostJournalResponse.java
@@ -9,6 +9,6 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PostJournalResponse {
-  String journalId;
+  String id;
   String message;
 }
