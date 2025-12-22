8a4ea23d275eebe3bd1305ac107480bc42311071
MdSujan99 <khans8962@gmail.com>
2025-12-22 23:32:26 +0530

    minor refactor

--- src/main/java/com/mds/journal_app/controller/JournalController.java
+++ src/main/java/com/mds/journal_app/controller/JournalController.java
@@ -7,14 +7,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/api/")
-@Slf4j
 @CrossOrigin(origins = "http://localhost:3000")
+@Slf4j
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/")
 public class JournalController {
   private final JournalService journalService;
 
